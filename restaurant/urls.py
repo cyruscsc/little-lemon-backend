4087f3f4e0bed41c2395b4fd31ec1918d10d5f12
Cyrus Chan <sairuscsc@gmail.com>
2023-08-15 18:54:44 -0400

    Remove templates and static files

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,7 +3,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-  path('', views.IndexView.as_view()),
   path('api/menu', views.MenuView.as_view()),
   path('api/menu/<int:pk>', views.MenuItemView.as_view()),
   path('api/bookings', views.BookingsView.as_view()),
