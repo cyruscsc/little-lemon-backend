e0a66901c7fd43b2fcbc71001dad8799b56c79a5
Cyrus Chan <sairuscsc@gmail.com>
2023-08-19 14:46:02 -0400

    update urls

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include('restaurant.urls')),
+    path('api/', include('restaurant.urls')),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,9 +3,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-  path('api/menu', views.MenuView.as_view()),
-  path('api/menu/<int:pk>', views.MenuItemView.as_view()),
-  path('api/bookings', views.BookingsView.as_view()),
-  path('api/bookings/<int:pk>', views.SingleBookingView.as_view()),
-  path('api-token-auth', obtain_auth_token),
+  path('csrf', views.csrfView),
+  path('register', views.RegisterView.as_view()),
+  path('login', views.loginView),
+  path('logout', views.logoutView),
+  path('session', views.SessionView.as_view()),
+  path('user', views.UserView.as_view()),
+  path('menu', views.MenuView.as_view()),
+  path('menu/<int:pk>', views.MenuItemView.as_view()),
+  path('bookings', views.BookingsView.as_view()),
+  path('bookings/<int:pk>', views.SingleBookingView.as_view()),
 ]
