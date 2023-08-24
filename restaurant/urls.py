ee35040bb6c694b62eede78b765b1eadca323154
Cyrus Chan <sairuscsc@gmail.com>
2023-08-23 22:52:58 -0400

    create views and urls for checking username and email; remove redundant code

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('checkusername', views.CheckUsernameView.as_view()),
+  path('checkemail', views.CheckEmailView.as_view()),
   path('register', views.RegisterView.as_view()),
   path('login', views.LoginView.as_view()),
   path('logout', views.LogoutView.as_view()),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,24 +1,43 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.http import JsonResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-from datetime import datetime
 from .models import Menu, Booking
 from .serializers import UserSerializer, MenuSerializer, BookingSerializer
-from .permissions import IsManager
 from .jwtauth import JwtToken, ACCESS_TOKEN_COOKIE_KEY, REFRESH_TOKEN_COOKIE_KEY
-import jwt, datetime
-
-jwt_secret = 'This is not a secret'
 
 # Create your views here.
+class CheckUsernameView(APIView):
+  def post(self, request, *args, **kwargs):
+    username = request.data['username']
+    if User.objects.filter(username=username).exists():
+      return Response({
+        'available': False,
+        'message': 'Username already in use'
+        })
+    else:
+      return Response({
+        'available': True,
+        'message': ''
+        })
+
+class CheckEmailView(APIView):
+  def post(self, request, *args, **kwargs):
+    email = request.data['email']
+    if User.objects.filter(email=email).exists():
+      return Response({
+        'available': False,
+        'message': 'Email already in use'
+        })
+    else:
+      return Response({
+        'available': True,
+        'message': ''
+        })
+
 class RegisterView(APIView):
   def post(self, request, *args, **kwargs):
     username = request.data['username']
@@ -35,6 +54,7 @@ class RegisterView(APIView):
     serializer.is_valid(raise_exception=True)
     serializer.save()
 
+    # Login user
     user = authenticate(username=username, password=password)
     jwt_token = JwtToken(request)
     jwt_token.issue(user.id, user.username, user.email, user.is_staff)
@@ -44,6 +64,10 @@ class RegisterView(APIView):
     response.set_cookie(key=REFRESH_TOKEN_COOKIE_KEY, value=jwt_token.refresh_token, httponly=True)
     response.data = {
       'message': f'User created, logged in as {username} successfully',
+      'user_id': user.id,
+      'username': user.username,
+      'email': user.email,
+      'is_staff': user.is_staff,
     }
     return response
 
@@ -64,14 +88,18 @@ class LoginView(APIView):
     response.set_cookie(key=REFRESH_TOKEN_COOKIE_KEY, value=jwt_token.refresh_token, httponly=True)
     response.data = {
       'message': f'Logged in as {username} successfully',
+      'user_id': user.id,
+      'username': user.username,
+      'email': user.email,
+      'is_staff': user.is_staff,
     }
     return response
 
 class LogoutView(APIView):
   def post(self, request, *args, **kwargs):
     response = Response()
-    response.delete_cookie(ACCESS_TOKEN_COOKIE_KEY)
-    response.delete_cookie(REFRESH_TOKEN_COOKIE_KEY)
+    response.set_cookie(key=ACCESS_TOKEN_COOKIE_KEY, value='', max_age=1, httponly=True)
+    response.set_cookie(key=REFRESH_TOKEN_COOKIE_KEY, value='', max_age=1, httponly=True)
     response.data = {
       'message': 'Logged out successfully',
     }
